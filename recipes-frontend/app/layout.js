487d7eec0c3a245d0faa659abd70d7cb7dd92712
dsokol3 <dsokol3@student.touro.edu>
2026-04-30 17:18:58 -0400

    feat: initialize Next.js frontend with Material UI and root layout structure

--- recipes-frontend/app/layout.js
+++ recipes-frontend/app/layout.js
@@ -1,16 +1,21 @@
 import NavBar from "@/components/NavBar";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import "./globals.css";
 
 export const metadata = {
   title: "RecipeShare",
   description: "Share and discover delicious recipes with RecipeShare, your go-to platform for culinary inspiration and community engagement.",
-}
+};
+
 export default function RootLayout({ children }) {
   return (
-      <html lang="en">
+    <html lang="en">
       <body>
-      <NavBar />
-      {children}
+        <AppRouterCacheProvider>
+          <NavBar />
+          {children}
+        </AppRouterCacheProvider>
       </body>
-      </html>
+    </html>
   );
 }
